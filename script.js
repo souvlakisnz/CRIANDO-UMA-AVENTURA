c1af82c1684e0e7224c917168c8c80f02c65859b
souvlakisnz <0000111781712xsp@al.educacao.sp.gov.br>
2024-09-02 12:29:48 -0300

    Versão 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorAll('.btn-proximo')
+
+avanca array.forEach(button => {
+    button.addEventListener ('click', function(){
+        const atual = document.querySelector('.')
+    })
+});
