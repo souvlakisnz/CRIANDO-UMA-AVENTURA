8d7b5fa0d3779fee4d1f958667f643c6345ede29
souvlakisnz <0000111781712xsp@al.educacao.sp.gov.br>
2024-09-16 12:32:26 -0300

    versao final

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const avanca = document.querySelectorAll('.btn-proximo')
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca array.forEach(button => {
-    button.addEventListener ('click', function(){
-        const atual = document.querySelector('.')
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
-});
+})
